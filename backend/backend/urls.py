b5bdcb459ff231e5cdd3cf021e99c47abce338dd
jamiz qamar <jamizalam1@gmail.com>
2026-05-27 13:47:21 +0530

    add url patterns

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,7 +17,18 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.home, name='root'),  
+    # The empty string '' means: when user visits / 
+    # show views.home
+
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
+
+
