8256498cc1b08d24b418b5a56ca94f554feda4bf
jamiz qamar <jamizalam1@gmail.com>
2026-06-09 23:28:28 +0530

    new

--- urls.py
+++ urls.py
@@ -31,6 +31,8 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('jamiz/', include('jamiz.urls')),  # Include URLs from the jamiz app
     path("__reload__/", include("django_browser_reload.urls")),  # For django-browser-reload
+
+    
 ]
 
 
